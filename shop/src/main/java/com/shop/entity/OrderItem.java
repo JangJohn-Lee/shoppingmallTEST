2c33eb2055831f904f446b6c8c6153dfc240adc6
Jang_John <121841741+JangJohn-Lee@users.noreply.github.com>
2025-06-10 11:42:30 +0900

    주문기능을 위한 카트, 주문, 아이템, 상태 추가
    Order, CartItem, OrderItem 엔티티 추가 및 엔티티간 매핑 추가,
    OrderStatus 콘스탄트 추가
    오더 리포지토리 추가 및 카트 테스트, 오더테스트.
    
    // 하이버네이트 오타로 인한 가독성 떨어짐 수정

---
+++ shop/src/main/java/com/shop/entity/OrderItem.java
@@ -0,0 +1,36 @@
+package com.shop.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+public class OrderItem{
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    private int orderPrice; //주문수량
+
+    private int count;      //수량
+
+    private LocalDateTime regTime;  //등록시간
+
+    private LocalDateTime updateTime;   //수정시간
+}
