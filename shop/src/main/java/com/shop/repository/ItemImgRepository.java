5566e8252db94bac8da4d3aa5c80e6c46204736a
Jang_John <121841741+JangJohn-Lee@users.noreply.github.com>
2025-06-18 16:31:31 +0900

    상품등록 마무리
    application-test.properties,application-properties 수정해서 상품저장 경로, 리소스 업로드 경로 추가
    ddl을 validate 로 변경
    FileService 클래스 추가
    WebMvcConfig 클래스 추가
    ItemImgRepository 인터페이스 추가
    ItemService클래스 추가
    
    ItemServiceTest 테스트 진행으로 저장되는 로직 테스트 완료.

---
+++ shop/src/main/java/com/shop/repository/ItemImgRepository.java
@@ -0,0 +1,12 @@
+package com.shop.repository;
+
+import com.shop.entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+
+    //매개변수로 넘겨준 상품아이디로 상품 이미지 아이디의 오름차순으로 가져오는 쿼리 메소드
+    List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
+}
