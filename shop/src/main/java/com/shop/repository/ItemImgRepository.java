5e9e0f9b2cdc300e371466df9b15863bce042a2b
Jang_John <121841741+JangJohn-Lee@users.noreply.github.com>
2025-08-02 07:31:00 +0900

    주문내역 확인하는 기능 추가
    controller단에서 자꾸 오타가나서 실수를 함...정신차리기

--- shop/src/main/java/com/shop/repository/ItemImgRepository.java
+++ shop/src/main/java/com/shop/repository/ItemImgRepository.java
@@ -9,4 +9,7 @@ public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
 
     //매개변수로 넘겨준 상품아이디로 상품 이미지 아이디의 오름차순으로 가져오는 쿼리 메소드
     List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);
+
+    //구매이력 페이지에서 주문상품의 대표 이미지 보여주기위해 상품의 대표 이미지를 찾는 쿼리 메소드 추가.
+    ItemImg findByItemIdAndRepimgYn(Long itemId, String repimgYn);
 }
