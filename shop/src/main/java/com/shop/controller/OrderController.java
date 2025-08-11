0aa44819acfbe6f0ea7451a06d7f553a2bff0bec
JangJohn-Lee <sangrock510@naver.com>
2025-08-12 05:58:55 +0900

    merge 실수로 인한 소실분 복구
    구매이력에서 주문취소까지 구현

--- shop/src/main/java/com/shop/controller/OrderController.java
+++ shop/src/main/java/com/shop/controller/OrderController.java
@@ -1,21 +1,24 @@
 package com.shop.controller;
 
 import com.shop.dto.OrderDto;
+import com.shop.dto.OrderHistDto;
 import com.shop.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,14 +27,14 @@ public class OrderController {
 
     private final OrderService orderService;
 
-    @PostMapping(value="/order")
+    @PostMapping(value = "/order")
     //스프링에서 비동기 처리할때 @RequestBody와 @ResponseBody 어노테이션 사용
     //@RequestBody:HTTP 요청의 본문 body에 담긴 내용을 자바 객체로 전달
     //@ResponseBody:자바 객체를 HTTP요청의 Body로 전달
-    public @ResponseBody ResponseEntity order (@RequestBody @Valid OrderDto orderDto, BindingResult bingResult, Principal principal){
+    public @ResponseBody ResponseEntity order(@RequestBody @Valid OrderDto orderDto, BindingResult bingResult, Principal principal) {
 
         //주문 정보를 받는 orderDto 객체에 데이터 바인딩 시 에러가 있는지 검사
-        if(bingResult.hasErrors()){
+        if (bingResult.hasErrors()) {
             StringBuilder sb = new StringBuilder();
             List<FieldError> fieldErrors = bingResult.getFieldErrors();
             for (FieldError fieldError : fieldErrors) {
@@ -42,25 +45,35 @@ public class OrderController {
 
         String email = principal.getName();
         Long orderId;
-        try{
+        try {
             orderId = orderService.order(orderDto, email);  //화면으로부터 넘어오는 주문 정보와 회원의 이메일 정보를 이용하여 주문 로직을 호출
-        }catch(Exception e){
+        } catch (Exception e) {
             return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
 
         return new ResponseEntity<Long>(orderId, HttpStatus.OK);    //결과값으로 생성된 주문 번호와 요청이 성공했다는 HTTP  응답 상태 코드를 반환합니다.
     }
 
-     @PostMapping("/order/{orderId}/cancel")
-    public @ResponseBody ResponseEntity cancelOrder (@PathVariable("orderId") Long orderId, Principal principal){
+    @GetMapping(value = {"/orders", "/orders/{page}"})
+    public String orderHist(@PathVariable("page") Optional<Integer> page, Principal principal, Model model) {
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 4);
+        Page<OrderHistDto> orderHistDtoList = orderService.getOrderList(principal.getName(), pageable);
+        model.addAttribute("orders", orderHistDtoList);
+        model.addAttribute("page", pageable.getPageNumber());
+        model.addAttribute("maxPage", 5);
+        return "order/orderHist";
+    }
+
+    @PostMapping("/order/{orderId}/cancel")
+    public @ResponseBody ResponseEntity cancelOrder(@PathVariable("orderId") Long orderId, Principal principal) {
         //자바스크립트에서 취소할 주문 번호는 조작이 가능하므로 다른 사람의 주문을 취소하지 못하도록 주문 취소 권한 검사
-        if(!orderService.validateOrder(orderId, principal.getName())){
+        if (!orderService.validateOrder(orderId, principal.getName())) {
             return new ResponseEntity<String>("주문 취소 권한이 없습니다.", HttpStatus.FORBIDDEN);
         }
 
         //주문 취소 로직 호출
         orderService.cancelOrder(orderId);
         return new ResponseEntity<Long>(orderId, HttpStatus.OK);
-     }
+    }
 
 }
--- shop/src/main/java/com/shop/service/OrderService.java
+++ shop/src/main/java/com/shop/service/OrderService.java
@@ -1,14 +1,17 @@
 package com.shop.service;
 
 import com.shop.dto.OrderDto;
-import com.shop.entity.Item;
-import com.shop.entity.Member;
-import com.shop.entity.Order;
-import com.shop.entity.OrderItem;
+import com.shop.dto.OrderHistDto;
+import com.shop.dto.OrderItemDto;
+import com.shop.entity.*;
+import com.shop.repository.ItemImgRepository;
 import com.shop.repository.ItemRepository;
 import com.shop.repository.MemberRepository;
 import com.shop.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.thymeleaf.util.StringUtils;
@@ -25,6 +28,7 @@ public class OrderService {
     private final ItemRepository itemRepository;
     private final MemberRepository memberRepository;
     private final OrderRepository orderRepository;
+    private final ItemImgRepository itemImgRepository;
 
     public Long order(OrderDto orderDto, String email) {
         Item item = itemRepository.findById(orderDto.getItemId()).orElseThrow(EntityNotFoundException::new);    //주문할 상품 조회
@@ -37,6 +41,34 @@ public class OrderService {
         orderRepository.save(order);    //생성한 주문 엔티티 저장하기`
 
         return order.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public Page<OrderHistDto> getOrderList(String email, Pageable pageable) {
+
+        //유저 아이디와 페이징조건으로 주문 목록 조회
+        List<Order> orders = orderRepository.findOrders(email, pageable);
+        Long totalCount = orderRepository.countOrder(email);    // 유저의 주문 총개수 구하기
+
+        List<OrderHistDto> orderHistDtos = new ArrayList<>();
+
+        //주문 리스트 순회하면서 구매 이력  페이지에 전달할 DTO 생성
+        for (Order order : orders) {
+            OrderHistDto orderHistDto = new OrderHistDto(order);
+            List<OrderItem> orderItems = order.getOrderItems();
+            for (OrderItem orderItem : orderItems) {
+                ItemImg itemImg = itemImgRepository.findByItemIdAndRepimgYn
+                        (orderItem.getItem().getId(), "Y"); // 주문한 상품 대표이미지 조회
+                OrderItemDto orderItemDto =
+                        new OrderItemDto(orderItem, itemImg.getImgUrl());
+                orderHistDto.addOrderItemDto(orderItemDto);
+            }
+
+            orderHistDtos.add(orderHistDto);
+        }
+
+        //페이지 구현 객체를 생성해서 반환
+        return new PageImpl<OrderHistDto>(orderHistDtos, pageable, totalCount);
     }
 
     //주문 취소하는 로직
